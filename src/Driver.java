ac7f9b9f06e4def90330c8e4367803c5c7204b87
deeswoc <daniel.lor.nelson@gmail.com>
2022-06-03 18:08:00 -0500

    Instantiated child classes

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,21 @@
-public class Driver {
+import java.util.ArrayList;
 
+public class Driver {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        Dog dog = new Dog();
+        Elephant elephant = new Elephant();
+        
+        List<Animal> animalList = new ArrayList<Animal>();
+        animalList.add(bird);
+        animalList.add(dog);
+        animalList.add(elephant);
+
+        for (Animal animal: animalList
+             ) {
+            animal.move();
+            animal.makeNoise();
+            aimal.eat("Pumpkin");
+        }
+    }
 }
